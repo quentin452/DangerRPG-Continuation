70b02b0fc0f015609aec7bcbd6e55d1f548c9207
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-11-05 17:26:08 +0100

    Fix #37

--- src/main/java/mixac1/dangerrpg/DangerRPG.java
+++ src/main/java/mixac1/dangerrpg/DangerRPG.java
@@ -24,7 +24,7 @@ public class DangerRPG {
 
     public static final String MODNAME = "DangerRPG";
     public static final String MODID = "dangerrpg";
-    public static final String VERSION = "${version}";
+    public static final String VERSION = "1.1.4";
     public static final String ACCEPTED_VERSION = "[1.7.10]";
 
     @Instance(DangerRPG.MODID)
