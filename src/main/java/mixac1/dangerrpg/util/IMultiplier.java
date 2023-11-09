de463492c9c4a23444ce73787739dafce8cd273a
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-11-09 07:00:29 +0100

    Update IMultiplier.java

--- src/main/java/mixac1/dangerrpg/util/IMultiplier.java
+++ src/main/java/mixac1/dangerrpg/util/IMultiplier.java
@@ -1,14 +1,15 @@
 package mixac1.dangerrpg.util;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public interface IMultiplier<Type> extends Serializable {
 
-    public Type up(Type value, Object... objs);
+    Type up(Type value, Object... objs);
 
-    public interface IMultiplierE<Type> extends IMultiplier<Type> {
+    interface IMultiplierE<Type> extends IMultiplier<Type> {
 
-        public Type down(Type value, Object... objs);
+        Type down(Type value, Object... objs);
     }
 
     enum MulType {
@@ -60,15 +61,13 @@ public interface IMultiplier<Type> extends Serializable {
             if (strs.length == 2) {
                 MulType type = MulType.valueOf(strs[0].toUpperCase());
                 Float value = Float.valueOf(strs[1]);
-                if (type != null && value != null) {
-                    return type.getMul(value);
-                }
+                return type.getMul(value);
             }
             return null;
         }
     }
 
-    public static abstract class Multiplier implements IMultiplierE<Float> {
+    abstract class Multiplier implements IMultiplierE<Float> {
 
         public final Float mul;
         public final MulType mulType;
@@ -84,7 +83,7 @@ public interface IMultiplier<Type> extends Serializable {
         }
     }
 
-    public static class MultiplierAdd extends Multiplier {
+    class MultiplierAdd extends Multiplier {
 
         public MultiplierAdd(Float mul) {
             super(mul, MulType.ADD);
@@ -101,7 +100,7 @@ public interface IMultiplier<Type> extends Serializable {
         }
     }
 
-    public static class MultiplierMul extends Multiplier {
+    class MultiplierMul extends Multiplier {
 
         public MultiplierMul(Float mul) {
             super(mul, MulType.MUL);
@@ -118,7 +117,7 @@ public interface IMultiplier<Type> extends Serializable {
         }
     }
 
-    public static class MultiplierSQRT extends Multiplier {
+    class MultiplierSQRT extends Multiplier {
 
         public MultiplierSQRT(Float mul) {
             super(mul, MulType.SQRT);
@@ -137,11 +136,11 @@ public interface IMultiplier<Type> extends Serializable {
             float d = (float) Math.sqrt((Math.pow(b, 2) - 4 * a * c));
             float tmp1 = (-b - d) / 2 * a;
             float tmp2 = (-b + d) / 2 * a;
-            return up(tmp1) == value ? tmp1 : tmp2;
+            return Objects.equals(up(tmp1), value) ? tmp1 : tmp2;
         }
     }
 
-    public static final MultiplierAdd ADD_1 = new MultiplierAdd(1F);
+    MultiplierAdd ADD_1 = new MultiplierAdd(1F);
 
-    public static final MultiplierMul MUL_1 = new MultiplierMul(1F);
+    MultiplierMul MUL_1 = new MultiplierMul(1F);
 }
