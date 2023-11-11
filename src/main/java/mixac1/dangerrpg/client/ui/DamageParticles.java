c985f38218af6535ad010c92845017c54e8e3ef4
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-11-11 02:13:00 +0100

    Update DamageParticles.java

--- src/main/java/mixac1/dangerrpg/client/ui/DamageParticles.java
+++ src/main/java/mixac1/dangerrpg/client/ui/DamageParticles.java
@@ -15,14 +15,15 @@ import org.lwjgl.opengl.GL11;
 @SideOnly(Side.CLIENT)
 public class DamageParticles extends EntityFX {
 
-    protected static final float GRAVITY = 0.1F;
-    protected static final int LIFESPAN = 12;
+    private static final Minecraft mc = Minecraft.getMinecraft();
+    private static final float GRAVITY = 0.1F;
+    private static final int LIFESPAN = 12;
+    private static final double PARTICLE_SCALE_FACTOR = 0.008D;
+    private static final double MAX_PARTICLE_SCALE = RPGConfig.ClientConfig.Data.size2 * 3.0D;
 
-    protected final String text;
-    protected final boolean shouldOnTop = true;
-    protected final float scale = 1.0F;
+    private final String text;
     private final int damage;
-    protected boolean grow = true;
+    private boolean grow = true;
 
     public DamageParticles(int damage, World world, double parX, double parY, double parZ, double parMotionX, double parMotionY, double parMotionZ) {
         super(world, parX, parY, parZ, parMotionX, parMotionY, parMotionZ);
@@ -40,65 +41,54 @@ public class DamageParticles extends EntityFX {
     }
 
     @Override
-    public void renderParticle(Tessellator p_70539_1_, float p_70539_2_, float p_70539_3_, float p_70539_4_, float p_70539_5_, float p_70539_6_, float p_70539_7_) {
-        float rotationYaw = (-Minecraft.getMinecraft().thePlayer.rotationYaw);
-        float rotationPitch = Minecraft.getMinecraft().thePlayer.rotationPitch;
-
-        float f11 = (float) (this.prevPosX + (this.posX - this.prevPosX) * (double) p_70539_2_ - interpPosX);
-        float f12 = (float) (this.prevPosY + (this.posY - this.prevPosY) * (double) p_70539_2_ - interpPosY);
-        float f13 = (float) (this.prevPosZ + (this.posZ - this.prevPosZ) * (double) p_70539_2_ - interpPosZ);
+    public void renderParticle(Tessellator tessellator, float partialTicks, float p_70539_3_, float p_70539_4_, float p_70539_5_, float p_70539_6_, float p_70539_7_) {
+        float f11 = (float) (prevPosX + (posX - prevPosX) * (double) partialTicks  - interpPosX);
+        float f12 = (float) (prevPosY + (posY - prevPosY) * (double) partialTicks  - interpPosY);
+        float f13 = (float) (prevPosZ + (posZ - prevPosZ) * (double) partialTicks  - interpPosZ);
 
         GL11.glPushMatrix();
-        if (this.shouldOnTop) {
-            GL11.glDepthFunc(519);
-        } else {
-            GL11.glDepthFunc(515);
-        }
+        GL11.glDepthFunc(519);
         GL11.glTranslatef(f11, f12, f13);
-        GL11.glRotatef(rotationYaw, 0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(rotationPitch, 1.0F, 0.0F, 0.0F);
-
+        GL11.glRotatef(-mc.thePlayer.rotationYaw, 0.0F, 1.0F, 0.0F);
+        GL11.glRotatef(mc.thePlayer.rotationPitch, 1.0F, 0.0F, 0.0F);
         GL11.glScalef(-1.0F, -1.0F, 1.0F);
-        GL11.glScaled(this.particleScale * 0.008D, this.particleScale * 0.008D, this.particleScale * 0.008D);
-        GL11.glScaled(this.scale, this.scale, this.scale);
+        float scale = 1.0F;
+        GL11.glScaled(particleScale * PARTICLE_SCALE_FACTOR * scale, particleScale * PARTICLE_SCALE_FACTOR * scale, particleScale * PARTICLE_SCALE_FACTOR * scale);
 
         OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 0.003662109F);
-        GL11.glEnable(3553);
-        GL11.glDisable(3042);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_BLEND);
         GL11.glDepthMask(true);
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        GL11.glEnable(3553);
-        GL11.glEnable(2929);
-        GL11.glDisable(2896);
-        GL11.glBlendFunc(770, 771);
-        GL11.glEnable(3042);
-        GL11.glEnable(3008);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glDisable(GL11.GL_CULL_FACE);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glEnable(GL11.GL_ALPHA_TEST);
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 
-        int color = RPGConfig.ClientConfig.Data.damageColor;
-        if (damage < 0) {
-            color = RPGConfig.ClientConfig.Data.healColor;
-        }
-
-        final FontRenderer fontRenderer = Minecraft.getMinecraft().fontRenderer;
-        fontRenderer.drawStringWithShadow(this.text, -MathHelper.floor_float(fontRenderer.getStringWidth(this.text) / 2.0F) + 1, -MathHelper.floor_float(fontRenderer.FONT_HEIGHT / 2.0F) + 1, color);
+        int color = damage < 0 ? RPGConfig.ClientConfig.Data.healColor : RPGConfig.ClientConfig.Data.damageColor;
+        final FontRenderer fontRenderer = mc.fontRenderer;
+        fontRenderer.drawStringWithShadow(text, -MathHelper.floor_float(fontRenderer.getStringWidth(text) / 2.0F) + 1, -MathHelper.floor_float(fontRenderer.FONT_HEIGHT / 2.0F) + 1, color);
 
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         GL11.glDepthFunc(515);
 
         GL11.glPopMatrix();
-        if (this.grow) {
-            this.particleScale *= 1.08F;
-            if (this.particleScale > RPGConfig.ClientConfig.Data.size2 * 3.0D) {
-                this.grow = false;
+
+        if (grow) {
+            particleScale *= 1.08F;
+            if (particleScale > MAX_PARTICLE_SCALE) {
+                grow = false;
             }
         } else {
-            this.particleScale *= 0.96F;
+            particleScale *= 0.96F;
         }
     }
 
+    @Override
     public int getFXLayer() {
         return 3;
     }
-
 }
